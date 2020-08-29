10b848567959f06ad23ae89afa821071238d990c
Brendan Moran <b.daniel.moran@gmail.com>
2020-08-29 19:32:53 -0400

    Added basic route handling

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+/*
+ * MODULES AND SETUP
+ */
+
 const express = require('express');
 const app = express();
-app.listen(3000);
+
+/*
+ * ROUTES
+ */
+
+app.get('/', (req, res) => {
+  res.send('<h1>Bingo.</h1>');
+});
+
+app.get('/jankt', (req, res) => {
+  res.send('<h1>Get jankt, dingus.</h1>');
+});
+
+/*
+ * RUN
+ */
+
+app.listen(3000, () => {
+  console.log('The app is running on localhost:3000!');
+});
