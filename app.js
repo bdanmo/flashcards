cd993b890e9fd86e14af90416d9921d7ae515487
Brendan Moran <b.daniel.moran@gmail.com>
2020-08-29 21:29:44 -0400

    Added locals to card route response

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cards', (req, res) => {
+  res.locals = {
+    prompt: "Who is buried in Grant's tomb?",
+    hint: 'Think about whose tomb it is.',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
+    name: 'Booster'
+  };
   res.render('card');
 });
 
