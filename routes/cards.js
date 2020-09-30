59318a7826652ee450b7481a988d9c47e19f2476
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-29 20:42:53 -0400

    removed card route from index to card.js

--- routes/cards.js
+++ routes/cards.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/cards', (req, res) => {
+  res.locals = {
+    prompt: "Who is buried in Grant's tomb?",
+    hint: 'Think about whose tomb it is.',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
+    name: 'Booster'
+  };
+  res.render('card');
+});
--- routes/index.js
+++ routes/index.js
@@ -22,16 +22,6 @@ router.get('/hello', (req, res) => {
   }
 });
 
-router.get('/cards', (req, res) => {
-  res.locals = {
-    prompt: "Who is buried in Grant's tomb?",
-    hint: 'Think about whose tomb it is.',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
-    name: 'Booster'
-  };
-  res.render('card');
-});
-
 router.get('/sandbox', (req, res) => {
   res.locals = { friends };
   res.render('sandbox');
