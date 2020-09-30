b6c7be9ec298c6c3fc05e59fd946139c4a30948f
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-29 20:35:45 -0400

    Modularizing the routes

--- app.js
+++ app.js
@@ -5,19 +5,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-const friends = [
-  { name: 'Chris McGuire', first: 'Chris', last: 'McGuire' },
-  { name: 'Stephen Horne', first: 'Stephen', last: 'Horne' },
-  { name: 'Benjamin McGrath', first: 'Benjamin', last: 'McGrath' },
-  { name: 'Ben Drake', first: 'Ben', last: 'Drake' },
-  { name: 'Christopher McGarvey', first: 'Christopher', last: 'McGarvey' }
-];
+/* ROUTER */
+app.use(routes);
 
 /*
  * CUSOTM MIDDLEWARE
@@ -26,56 +22,6 @@ const friends = [
 app.use((req, res, next) => {
   console.log('Hello');
   next();
-});
-
-/*
- * GET ROUTES
- */
-
-app.get('/', (req, res) => {
-  const username = req.cookies.username;
-  if (username) {
-    res.render('index', { username });
-  } else {
-    res.redirect('/hello');
-  }
-});
-
-app.get('/hello', (req, res) => {
-  if (req.cookies.username) {
-    res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-app.get('/cards', (req, res) => {
-  res.locals = {
-    prompt: "Who is buried in Grant's tomb?",
-    hint: 'Think about whose tomb it is.',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
-    name: 'Booster'
-  };
-  res.render('card');
-});
-
-app.get('/sandbox', (req, res) => {
-  res.locals = { friends };
-  res.render('sandbox');
-});
-
-/*
- * POST ROUTES
- */
-
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username); //req.body.username refers to form input
-  res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
 });
 
 /*
---
+++ routes/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const { friends } = require('../package.json');
+
+/*
+ * GET ROUTES
+ */
+
+router.get('/', (req, res) => {
+  const username = req.cookies.username;
+  if (username) {
+    res.render('index', { username });
+  } else {
+    res.redirect('/hello');
+  }
+});
+
+router.get('/hello', (req, res) => {
+  if (req.cookies.username) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
+
+router.get('/cards', (req, res) => {
+  res.locals = {
+    prompt: "Who is buried in Grant's tomb?",
+    hint: 'Think about whose tomb it is.',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
+    name: 'Booster'
+  };
+  res.render('card');
+});
+
+router.get('/sandbox', (req, res) => {
+  res.locals = { friends };
+  res.render('sandbox');
+});
+
+/*
+ * POST ROUTES
+ */
+
+router.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username); //req.body.username refers to form input
+  res.redirect('/');
+});
+
+router.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
+
+module.exports = router;
