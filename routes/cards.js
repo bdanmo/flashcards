5217245945cac0a3af8568230cf8768d53e97d8e
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-30 18:27:14 -0400

    Set up query string and updated card template for new local

--- routes/cards.js
+++ routes/cards.js
@@ -5,9 +5,9 @@ const { cards } = data;
 
 router.get('/:id', (req, res) => {
   let { id } = req.params;
-  let { side } = req.query;
+  let { side } = req.query; //will refer to question or answer in card data
   res.locals = {
-    prompt: cards[id].question,
+    text: cards[id][side],
     hint: cards[id].hint,
     answer: cards[id].answer,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
