7a83653f1f3dd92c45e313ca8f6fcd4afd5fab1e
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-30 17:57:00 -0400

    moved cards.json to data folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { friends } = require('../friends.json');
+const { friends } = require('../data/friends.json');
 
 /*
  * GET ROUTES
