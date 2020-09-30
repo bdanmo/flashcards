ac3fb2087ef98466bfd6f1392b913adea378b35d
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-30 19:20:35 -0400

    Enabled side switching on card template

--- routes/cards.js
+++ routes/cards.js
@@ -8,6 +8,8 @@ router.get('/:id', (req, res) => {
   const { side } = req.query; //will refer to question or answer in card data
 
   res.locals = {
+    id,
+    side,
     text: cards[id][side],
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
     name: 'Booster'
