a7fee59e0780c9c0887a725f3d940b6956035ba8
Brendan Moran <b.daniel.moran@gmail.com>
2020-09-30 19:02:39 -0400

    Refined the card/:id route to only show hint on question side

--- routes/cards.js
+++ routes/cards.js
@@ -4,15 +4,19 @@ const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
 router.get('/:id', (req, res) => {
-  let { id } = req.params;
-  let { side } = req.query; //will refer to question or answer in card data
+  const { id } = req.params;
+  const { side } = req.query; //will refer to question or answer in card data
+
   res.locals = {
     text: cards[id][side],
-    hint: cards[id].hint,
-    answer: cards[id].answer,
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0F7y3MjYFAS-BkaeNyMUAxRNKqoSKkYXn9A&usqp=CAU',
     name: 'Booster'
   };
+
+  if (side === 'question') {
+    res.locals.hint = cards[id][side];
+  }
+
   res.render('card');
 });
 
