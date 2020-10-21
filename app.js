5921d81ae7c07e250b014c15fdd79bef388c16ae
Brendan Moran <b.daniel.moran@gmail.com>
2020-10-20 21:15:19 -0400

    Added static files (img and css)

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cardRoutes = require('./routes/cards');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 /* ROUTER */
